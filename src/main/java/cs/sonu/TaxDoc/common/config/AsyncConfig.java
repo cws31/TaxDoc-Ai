06234ee9f286510aca93524436a883d4e004e72a
sonu kumar <gautamrocky909621@gmail.com>
2026-08-16 16:17:49 +0530

    feat(config): implement async and jackson configurations for entry pipeline
    impplement the asynchronous and jacson configuration to support asynchrounous for entri pipeline from one place

---
+++ src/main/java/cs/sonu/TaxDoc/common/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package cs.sonu.TaxDoc.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "documentTaskExecutor")
+    public Executor documentTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("TaxDoc-Async-");
+
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
+}
